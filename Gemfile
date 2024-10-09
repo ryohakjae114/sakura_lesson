7084316149ca6e5fefede8d0784a1bfa49f46128
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-10-09 11:47:45 +0900

    letter_openerインストール

--- Gemfile
+++ Gemfile
@@ -31,6 +31,7 @@ group :development do
   gem 'haml_lint'
   # NOTE: rails haml:erb2hamlをするために必要だった
   gem 'html2haml'
+  gem 'letter_opener_web'
   gem 'sgcop', github: 'SonicGarden/sgcop'
   gem 'web-console'
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root 'lessons#index'
   get 'up' => 'rails/health#show', as: :rails_health_check
+
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: '/letter_opener'
+  end
 end
