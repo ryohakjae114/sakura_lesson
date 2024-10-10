2a059afa4358845d4f180631727be9062f62de15
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-10-10 18:19:29 +0900

    レッスン開催日の登録機能を作成

--- app/controllers/admins/lessons/lesson_dates_controller.rb
+++ app/controllers/admins/lessons/lesson_dates_controller.rb
@@ -1,8 +1,30 @@
 class Admins::Lessons::LessonDatesController < Admins::ApplicationController
+  before_action :set_lesson
+
   def index
-    @lesson_dates = Lesson.find(params[:lesson_id]).lesson_dates.order(created_at: :desc).page(params[:page]).per(100)
+    @lesson_dates = @lesson.lesson_dates.order(created_at: :desc).page(params[:page]).per(100)
   end
 
   def new
+    @lesson_date = @lesson.lesson_dates.build
+  end
+
+  def create
+    @lesson_date = @lesson.lesson_dates.build(lesson_date_params)
+    if @lesson.save
+      redirect_to admins_lesson_lesson_dates_path(@lesson), notice: t('controller.created')
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_lesson
+    @lesson = Lesson.find(params[:lesson_id])
+  end
+
+  def lesson_date_params
+    params.require(:lesson_date).permit(:date, :start_time, :end_time, :capacity, :url)
   end
 end
