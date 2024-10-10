ffa33d2ca3e80e87f9934aeeb9241632e2a2a615
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-10-10 18:42:55 +0900

    レッスン開催日を登録できることのテスト作成

--- spec/system/admins/lessons/admins_lessons_lesson_dates_spec.rb
+++ spec/system/admins/lessons/admins_lessons_lesson_dates_spec.rb
@@ -20,4 +20,23 @@ RSpec.describe 'Admins::Lessons::LessonDates', type: :system do
     expect(page).to have_content '50'
     expect(page).to have_content 'https://example.com/example'
   end
+
+  it 'レッスン開催日を登録できること' do
+    tomorrow = Time.zone.tomorrow
+    visit admins_lesson_lesson_dates_path(lesson)
+    click_on 'レッスン開催日登録'
+    select tomorrow.year, from: 'lesson_date[date(1i)]'
+    select tomorrow.month, from: 'lesson_date[date(2i)]'
+    select tomorrow.day, from: 'lesson_date[date(3i)]'
+    select '13', from: 'lesson_date[start_time(4i)]'
+    select '00', from: 'lesson_date[start_time(5i)]'
+    select '15', from: 'lesson_date[end_time(4i)]'
+    select '00', from: 'lesson_date[end_time(5i)]'
+    fill_in '定員', with: 50
+    fill_in 'リンク', with: 'https://example.com/meeting_url'
+    expect do
+      click_on '登録する'
+    end.to change(lesson.lesson_dates, :count).by(1)
+    expect(page).to have_content '新規登録しました'
+  end
 end
