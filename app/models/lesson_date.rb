1abf9c0f87370964fbd7c267629e3aa2241e5aef
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-10-10 12:35:19 +0900

    lessonとlessondateを紐付け

--- app/models/lesson_date.rb
+++ app/models/lesson_date.rb
@@ -1,2 +1,9 @@
 class LessonDate < ApplicationRecord
+  belongs_to :lesson
+
+  validates :date, presence: true, date: true
+  validates :start_time, presence: true
+  validates :end_time, presence: true
+  validates :capacity, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
+  validates :url, presence: true, format: /\A#{URI::DEFAULT_PARSER.make_regexp(%w[http https])}\z/
 end
