0a47c23e372fdfd4cbcb2c60d8c0c2aff16a4229
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-10-10 18:20:48 +0900

    レッスン開催日の文言の修正

--- spec/system/admins/lessons/admins_lessons_lesson_dates_spec.rb
+++ spec/system/admins/lessons/admins_lessons_lesson_dates_spec.rb
@@ -8,11 +8,11 @@ RSpec.describe 'Admins::Lessons::LessonDates', type: :system do
     sign_in hakjae
   end
 
-  it 'レッスン日時を一覧できること' do
+  it 'レッスン開催日を一覧できること' do
     create(:lesson_date, date: '2025-10-10', start_time: '12:00:00', end_time: '15:00:00', capacity: 50, url: 'https://example.com/example', lesson:)
     visit admins_root_path
     within(".test_#{lesson.id}") do
-      click_on 'レッスン日時一覧'
+      click_on 'レッスン開催日一覧'
     end
     expect(page).to have_content '2025-10-10'
     expect(page).to have_content '12:00'
