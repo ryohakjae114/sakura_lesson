b80f1b6bcb42edb0094462deb3b7482ea31aa841
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-10-10 19:01:02 +0900

    同じレッスンと同じ開始日のレッスン日は作成できないことのテスト作成

--- spec/factories/lesson_dates.rb
+++ spec/factories/lesson_dates.rb
@@ -1,9 +1,10 @@
 FactoryBot.define do
   factory :lesson_date do
-    date { '2024-10-10' }
-    start_time { '2024-10-10 12:08:39' }
-    end_time { '2024-10-10 12:08:39' }
-    capacity { 1 }
-    url { 'MyString' }
+    date { Time.zone.tomorrow }
+    start_time { '12:00' }
+    end_time { '15:00' }
+    capacity { 20 }
+    url { 'https://example.com' }
+    lesson
   end
 end
