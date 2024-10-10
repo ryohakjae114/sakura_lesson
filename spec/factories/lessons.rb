9e913f2cd8c6695637e7e92d87063c742720bb68
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-10-10 11:12:50 +0900

    lessonの定員カラムを削除

--- spec/factories/lessons.rb
+++ spec/factories/lessons.rb
@@ -2,7 +2,6 @@ FactoryBot.define do
   factory :lesson do
     title { '英語' }
     summary { '実用的な英語を学びたい人、ここに集え' }
-    maximum_allowed { 1 }
     instructor { 'MyString' }
     public { false }
   end
