01b3785fdaf2da4f629baa5bbb4e56ce92bbd0b1
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-10-11 12:09:42 +0900

    レッスン開催日の一覧機能のテスト作成

---
+++ spec/system/lessons/lessons_lesson_dates_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe 'Lessons::LessonDates', type: :system do
+  let!(:hakjae) { create(:admin) }
+  let!(:lesson) { create(:lesson, title: 'カヤック', instructor: '阿久津', published: true) }
+
+  it 'レッスン開催日を一覧できること' do
+    create(:lesson_date, date: '2025-10-10', lesson:)
+    create(:lesson_date, date: '2025-10-11', lesson:)
+    visit lesson_path(lesson)
+    click_on 'このレッスンの開催日時を確認する'
+    expect(page).to have_content 'レッスン開催日一覧'
+    expect(page).to have_content '2025-10-10'
+    expect(page).to have_content '2025-10-11'
+  end
+end
