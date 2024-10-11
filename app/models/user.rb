3c8b33bd0b9a7dd1672dcdc9bbd3838dbe93bae6
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-10-11 09:40:21 +0900

    アカウント作成時にnameを入力必須とする

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable, :rememberable, :validatable
+
+  validates :name, presence: true, length: { maximum: 200 }
 end
