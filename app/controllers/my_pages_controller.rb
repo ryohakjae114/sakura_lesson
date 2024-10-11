f290cb9ceb3a0a4b061a910d05732f9c354d53d8
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-10-11 17:24:25 +0900

    マイページで予約したレッスンを閲覧できる

--- app/controllers/my_pages_controller.rb
+++ app/controllers/my_pages_controller.rb
@@ -1,4 +1,5 @@
 class MyPagesController < ApplicationController
   def show
+    @reserved_lesson_dates = current_user.reserved_lesson_dates.order(date: :desc).page(params[:page]).per(10).includes(:lesson)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :confirmable, :rememberable, :validatable
 
   has_many :reservations, dependent: :destroy
+  has_many :reserved_lesson_dates, through: :reservations, source: :lesson_date
 
   validates :name, presence: true, length: { maximum: 200 }
 end
