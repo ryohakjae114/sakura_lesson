448e47d0d614d309fcbe0281d518416c6863107b
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-10-11 10:59:16 +0900

    レッスン詳細画面を作成

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,5 +1,16 @@
 class LessonsController < ApplicationController
+  before_action :set_lesson, only: %i[show]
+
   def index
     @lessons = Lesson.where(public: true).order(:created_at).page(params[:page]).per(10)
+  end
+
+  def show
+  end
+
+  private
+
+  def set_lesson
+    @lesson = Lesson.where(public: true).find(params[:id])
   end
 end
