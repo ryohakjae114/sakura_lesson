ea91b82d625906e56a9403c7ba59835e1b0d41ab
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-10-11 11:37:32 +0900

    title_domヘルパーを作成

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def admin_title_text(title)
     "#{Admin.model_name.human}#{title}"
   end
+
+  def title_dom(title)
+    content_tag(:h1, title, class: 'my-5')
+  end
 end
