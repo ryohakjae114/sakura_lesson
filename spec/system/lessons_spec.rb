891eb01370410ba3666b53e51f7329ee57885a53
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-10-11 11:04:42 +0900

    ユーザのレッスン詳細ページのテスト作成

--- spec/system/lessons_spec.rb
+++ spec/system/lessons_spec.rb
@@ -1,12 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe 'Lessons', type: :system do
-  let!(:hakjae) { create(:admin) }
-
-  before do
-    sign_in hakjae
-  end
-
   it 'レッスンを一覧できること' do
     create(:lesson, title: '算数', summary: '誰でも算数できるようになります', public: true)
     create(:lesson, title: '英語', summary: '誰でも英語できるようになります', public: true)
@@ -19,5 +13,15 @@ RSpec.describe 'Lessons', type: :system do
     expect(page).to have_content '英語'
     expect(page).to have_content '誰でも英語できるようになります'
     expect(page).not_to have_content '魔法'
+  end
+
+  it 'レッスン詳細ページを閲覧できること' do
+    create(:lesson, title: '算数', summary: '誰でも算数できるようになります', instructor: '林田もやし', public: true)
+    visit root_path
+    click_on 'レッスン詳細'
+    expect(page).to have_content 'レッスン詳細'
+    expect(page).to have_content '算数'
+    expect(page).to have_content '誰でも算数できるようになります'
+    expect(page).to have_content '林田もやし'
   end
 end
