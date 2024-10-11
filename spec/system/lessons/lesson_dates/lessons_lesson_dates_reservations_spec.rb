2266ee510756a1730e9fce5549748b731c483f63
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-10-11 18:34:59 +0900

    予約キャンセル機能のテスト作成

--- spec/system/lessons/lesson_dates/lessons_lesson_dates_reservations_spec.rb
+++ spec/system/lessons/lesson_dates/lessons_lesson_dates_reservations_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe 'Lessons::LessonDates::Reservations', type: :system do
   let!(:hakjae) { create(:user) }
   let!(:lesson) { create(:lesson, published: true, title: 'そろばん') }
+  let!(:lesson_date) { create(:lesson_date, lesson:, start_time: '15:00', end_time: '18:00') }
 
   context 'ログイン時' do
     before do
@@ -11,7 +12,6 @@ RSpec.describe 'Lessons::LessonDates::Reservations', type: :system do
     end
 
     it 'レッスン開催日に登録できること' do
-      create(:lesson_date, lesson:, start_time: '15:00', end_time: '18:00')
       visit lesson_lesson_dates_path(lesson)
       expect(page).not_to have_content '予約済み'
       expect do
@@ -23,11 +23,19 @@ RSpec.describe 'Lessons::LessonDates::Reservations', type: :system do
       expect(page).to have_content '15:00'
       expect(page).to have_content '18:00'
     end
+
+    it 'レッスン開催日の予約を削除できること' do
+      create(:reservation, lesson_date:, user: hakjae)
+      visit my_page_path
+      expect do
+        click_on '予約キャンセル'
+      end.to change(hakjae.reservations, :count).by(-1)
+      expect(page).to have_content '予約をキャンセルしました。'
+    end
   end
 
   context 'ログアウト時' do
     it 'レッスン開催日に登録できずログイン画面に遷移すること' do
-      create(:lesson_date, lesson:)
       visit lesson_lesson_dates_path(lesson)
       expect do
         click_on '予約する'
