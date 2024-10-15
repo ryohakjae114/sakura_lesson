26638d1387a646155e1072c954fc2f94813b4a1e
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-10-15 20:07:11 +0900

    現在の時間よりも前のレッスンは予約できない

--- spec/models/lesson_date_spec.rb
+++ spec/models/lesson_date_spec.rb
@@ -7,11 +7,18 @@ RSpec.describe LessonDate, type: :model do
     create(:lesson_date, start_at: '2024-10-17-12:00:00', end_at: '2024-10-17-13:00:00', capacity: 50, lesson:)
   end
 
-  it '他の開催日と開始時間と終了時間が被ってはいけない' do
+  it '現在の時間よりも前のレッスンは登録できない' do
     lesson_date = build(:lesson_date, start_at: '2024-10-17-11:00:00', end_at: '2024-10-17-12:00:00', lesson:)
     expect(lesson_date).to be_valid
+    lesson_date = build(:lesson_date, start_at: '2024-10-17-12:00:00', end_at: '2024-10-17-11:00:00', lesson:)
+    expect(lesson_date).not_to be_valid
+  end
+
+  it '他の開催日と開始時間と終了時間が被ってはいけない' do
     lesson_date = build(:lesson_date, start_at: '2024-10-17-11:00:00', end_at: '2024-10-17-12:01:00', lesson:)
     expect(lesson_date).not_to be_valid
+    lesson_date = build(:lesson_date, start_at: '2024-10-17-11:00:00', end_at: '2024-10-17-12:00:00', lesson:)
+    expect(lesson_date).to be_valid
 
     lesson_date = build(:lesson_date, start_at: '2024-10-17-12:59:00', end_at: '2024-10-17-14:00:00', lesson:)
     expect(lesson_date).not_to be_valid
