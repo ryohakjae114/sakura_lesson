e397124742135d28f30d71f3c54d6f609d225482
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-10-15 11:48:21 +0900

    レッスン編集と削除機能のテスト作成

--- spec/system/admins/admins_lessons_spec.rb
+++ spec/system/admins/admins_lessons_spec.rb
@@ -29,4 +29,30 @@ RSpec.describe 'Admins::Lessons', type: :system do
     expect(page).to have_content 'カジサック'
     expect(page).to have_content '野々村'
   end
+
+  it 'レッスンを編集できること' do
+    lesson = create(:lesson, title: 'カヤック', summary: 'いつでもカヤックしてます', instructor: '阿久津', published: false)
+    visit edit_admins_lesson_path(lesson)
+    fill_in 'レッスン名', with: 'カジサック'
+    fill_in '概要', with: 'どこよりもカヤックしてます'
+    fill_in '講師', with: '孫先生'
+    check '公開'
+    click_on '更新する'
+    expect(page).to have_content '更新しました'
+    expect(page).to have_field 'レッスン名', with: 'カジサック'
+    expect(page).to have_field '概要', with: 'どこよりもカヤックしてます'
+    expect(page).to have_field '講師', with: '孫先生'
+    expect(page).to have_checked_field '公開'
+  end
+
+  it 'レッスンを削除できること' do
+    create(:lesson, title: 'カヤック', summary: 'いつでもカヤックしてます', instructor: '阿久津', published: false)
+    visit admins_root_path
+    expect(page).to have_content 'カヤック'
+    click_on '編集'
+    expect do
+      click_on '削除'
+    end.to change(Lesson, :count).by(-1)
+    expect(page).to have_content '削除しました'
+  end
 end
