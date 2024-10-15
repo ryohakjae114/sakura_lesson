0833f1bcc5988ba7548435459c086535f06a6147
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-10-15 16:51:19 +0900

    カラムのデフォルト値の変更

--- db/migrate/20241015065027_add_columns_to_lesson_date.rb
+++ db/migrate/20241015065027_add_columns_to_lesson_date.rb
@@ -1,6 +1,6 @@
 class AddColumnsToLessonDate < ActiveRecord::Migration[7.2]
   def change
-    add_column :lesson_dates, :start_at, :datetime
-    add_column :lesson_dates, :end_at, :datetime
+    add_column :lesson_dates, :start_at, :datetime, null: false, default: -> { 'NOW()' }
+    add_column :lesson_dates, :end_at, :datetime, null: false, default: -> { 'NOW()' }
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -60,8 +60,8 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_15_065027) do
     t.text "url", default: "", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.datetime "start_at"
-    t.datetime "end_at"
+    t.datetime "start_at", default: -> { "now()" }, null: false
+    t.datetime "end_at", default: -> { "now()" }, null: false
     t.index ["lesson_id", "date"], name: "index_lesson_dates_on_lesson_id_and_date", unique: true
     t.index ["lesson_id"], name: "index_lesson_dates_on_lesson_id"
   end
