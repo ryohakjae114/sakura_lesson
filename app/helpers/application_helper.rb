0f64884b39ec0ebcaad3e7722af3b70c50832b92
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-10-15 12:19:19 +0900

    ユーザ一覧機能を作成

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,13 @@ module ApplicationHelper
   def title_dom(title)
     content_tag(:h1, title, class: 'my-5')
   end
+
+  def user_confirm_status_badge(user)
+    if user.confirmed?
+      content_tag(:span, t('admins.users.user_confirmed'),
+                  class: 'badge text-bg-success')
+    else
+      content_tag(:span, t('admins.users.user_unconfirmed'), class: 'badge text-bg-secondary')
+    end
+  end
 end
