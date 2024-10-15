0da4412dba2f65fe9b47b2ad550ae94770e308ce
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-10-15 17:28:00 +0900

    start_timeが今の時刻より前、もしくは start_timeがend_timeより後だったら保存できないように

--- app/models/lesson_date.rb
+++ app/models/lesson_date.rb
@@ -3,8 +3,10 @@ class LessonDate < ApplicationRecord
 
   has_many :reservations, dependent: :destroy
 
-  validates :start_at, presence: true
-  validates :end_at, presence: true
+  validates :start_at, presence: true,
+                       comparison: { greater_than_or_equal_to: Time.zone.now, message: I18n.t('activerecord.errors.models.lesson_dates.must_greater_than_now') }
+  validates :end_at, presence: true,
+                     comparison: { greater_than: :start_at, message: I18n.t('activerecord.errors.models.lesson_dates.must_greater_than_start_at') }
   validates :capacity, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
   validates :url, presence: true, format: /\A#{URI::DEFAULT_PARSER.make_regexp(%w[http https])}\z/
 end
