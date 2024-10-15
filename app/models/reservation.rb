4b73703ff805e9568586a359e6922f0c5376a18c
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-10-15 22:33:38 +0900

    レッスン開催日の予約がいっぱいなら予約できないように

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,4 +3,9 @@ class Reservation < ApplicationRecord
   belongs_to :lesson_date, counter_cache: true
 
   validates :user_id, uniqueness: { scope: :lesson_date_id }
+  validate :reservation_counts_must_be_within_capacity
+
+  def reservation_counts_must_be_within_capacity
+    errors.add(:base, 'reservation_counts_must_be_within_capacity') if lesson_date.reservations_count >= lesson_date.capacity
+  end
 end
