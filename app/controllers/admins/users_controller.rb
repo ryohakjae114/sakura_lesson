4acc467123de051f26fdab5098f4c5553e9e9af7
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-10-15 14:31:34 +0900

    ユーザ削除機能を作成

--- app/controllers/admins/users_controller.rb
+++ app/controllers/admins/users_controller.rb
@@ -1,5 +1,22 @@
 class Admins::UsersController < Admins::ApplicationController
+  before_action :set_user, only: %i[destroy]
+
   def index
     @users = User.order(created_at: :desc).page(params[:page]).per(100)
+  end
+
+  def destroy
+    @user.destroy!
+    redirect_to admins_root_path, notice: t('controller.destroyed'), status: :see_other
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :name)
   end
 end
