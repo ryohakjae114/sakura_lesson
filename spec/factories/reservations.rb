858182919702e5674b61a11b131db8f1f9359c73
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-10-16 12:02:21 +0900

    現在の時間よりも前のレッスンの予約は登録できないところのテスト作成

--- spec/factories/reservations.rb
+++ spec/factories/reservations.rb
@@ -2,5 +2,8 @@ FactoryBot.define do
   factory :reservation do
     user
     lesson_date
+    trait :skip_validation do
+      to_create { |instance| instance.save(validate: false) }
+    end
   end
 end
