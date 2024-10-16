d3907866e64d73a0fcc1263ab54fc89d119055b9
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-10-16 09:52:52 +0900

    現在の時間よりも前のレッスンを予約しても、予約できないように

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -8,4 +8,8 @@ class Reservation < ApplicationRecord
   def reservation_counts_must_be_within_capacity
     errors.add(:base, 'reservation_counts_must_be_within_capacity') if lesson_date.reservations_count >= lesson_date.capacity
   end
+
+  def cannot_reserve_already_started_lesson
+    errors.add(:base, 'cannot_reserve_already_started_lesson') if lesson_date.start_at < Time.zone.now
+  end
 end
