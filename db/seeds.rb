1f1909b4f1bc0326ec83cf0dded65752197923d3
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-10-17 13:02:16 +0900

    seedファイルを修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,8 @@
   title = Faker::Sport.sport(include_ancient: true)
   summary = Faker::Lorem.sentence(word_count: rand(3..20))
   instructor = Faker::Name.name
-  lesson = Lesson.create!(title:, summary:, instructor:)
+  published = rand(1..2) == 1
+  lesson = Lesson.create!(title:, summary:, instructor:, published:)
   start_at = Time.zone.now + rand(1..6)
   end_at = start_at + rand(1..6)
   lesson.lesson_dates.create!(start_at:, end_at:, capacity: rand(1..100), url: "https://example.com/#{('a'..'z').to_a.sample(8).join}")
